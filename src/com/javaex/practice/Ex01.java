e967c00b89341a55a7bfc053bfd31d72c114e55a
aassdd236 <aassdd236@naver.com>
2023-12-31 21:29:13 +0900

    1, 2

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -3,8 +3,19 @@ package com.javaex.practice;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] intArray = new int[5];
+		intArray[0]=3;
+		intArray[1]=7;
+		intArray[2]=12;
+		
+		int result=0;
+		
+		//0부터 length까자 하면 실제 배열의 크기보다 1 큼
+		for(int i=0; i<intArray.length; i++) {
+			result = result + intArray[i];
+		}
 
+		System.out.println(result);
 	}
 
 }
