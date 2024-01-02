487edc5586d28cd327635e34b3fc8487a1d9eeb6
aassdd236 <aassdd236@naver.com>
2024-01-02 12:38:14 +0900

    8번

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -4,13 +4,23 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 		int[] num =new int[6];
-		
+		boolean a;
+
+		do {
+			a = false;
+			for(int i=0; i<6; i++) {
+				num[i]=(int) (Math.random() *45) + 1;
+				for(int j=0; j<i; j++) {
+					if(num[i]==num[j]) {
+						a=true;
+					}
+				}
+			}
+		}while(a);
 		for(int i=0; i<6; i++) {
-			num[i]=(int) (Math.random() *45) + 1;
-			System.out.print(num[i] + "  ");
+			System.out.print(num[i] +" ");
 		}
 
-		
 	}
 
 }
